71090a86ec9e4981d15037fa5cc731bd97846de1
martabeveridge <marta@beveridge.uk>
2018-03-07 16:55:42 +0000

    added 2nd function

---
+++ lab_start_point/my_functions.rb
@@ -0,0 +1,11 @@
+def add_array_lengths(array_1, array_2)
+  return array_1.length + array_2.length
+end
+
+def sum_array(numbers)
+  total = 0
+    for num in numbers
+      total += num
+    end
+  return total
+end
